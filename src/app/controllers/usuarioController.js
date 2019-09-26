13297f80c2fac51ddeba3e5c371edb23984bec9a
Felipe Rodrigo <lipedigo@hotmail.com>
2019-09-26 16:43:33 -0300

    Adicionando rotas de Usuario

---
+++ src/app/controllers/usuarioController.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const authMiddleware = require('../middlewares/auth');
+
+const Usuario = require('../models/Usuario');
+
+const router = express.Router();
+
+router.use(authMiddleware);
+
+router.get('/selectAll', async (req, res) =>{
+    try{
+        const usuario = await Usuario.find()
+        
+        return res.send({ usuario });
+    } catch (err){
+        return res.status(400).send({ error: 'Erro ao carregar Usuarios' });
+    }
+})
+
+router.get('/selectById/:usuarioId', async (req, res) => {
+    try{
+        const usuario = await Usuario.findById(req.params.usuarioId);
+        
+        return res.send({ usuario });
+    } catch (err){
+        return res.status(400).send({ error: 'Erro ao carregar o Usuario' });
+    }
+})
+
+router.delete('/deleteById/:usuarioId', async (req, res) => {
+    try{
+        await Usuario.findByIdAndRemove(req.params.usuarioId);
+        
+        return res.send();
+    } catch (err){
+        return res.status(400).send({ error: 'Erro ao remover o usuario' });
+    }
+})
+
+module.exports = app => app.use('/usuario', router);
